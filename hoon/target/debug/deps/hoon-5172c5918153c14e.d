628207bb94b9b9f9d95f75be7e827e19d257e8d9
younghoonyou <kcnoh2@hanyang.ac.kr>
2022-02-07 00:16:45 +0900

    Cargo Build

---
+++ hoon/target/debug/deps/hoon-5172c5918153c14e.d
@@ -0,0 +1,5 @@
+/Users/yooyounghoon/Desktop/rust/hoon/target/debug/deps/hoon-5172c5918153c14e: src/main.rs
+
+/Users/yooyounghoon/Desktop/rust/hoon/target/debug/deps/hoon-5172c5918153c14e.d: src/main.rs
+
+src/main.rs:
---
+++ hoon/target/debug/hoon.d
@@ -0,0 +1,1 @@
+/Users/yooyounghoon/Desktop/rust/hoon/target/debug/hoon: /Users/yooyounghoon/Desktop/rust/hoon/src/main.rs
