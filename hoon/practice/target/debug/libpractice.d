462b476d86e963a8df3602b79e5246e0538fe658
younghoonyou <kcnoh2@hanyang.ac.kr>
2022-02-07 23:56:50 +0900

    Cargo Struct

---
+++ hoon/practice/target/debug/libpractice.d
@@ -0,0 +1,1 @@
+/Users/yooyounghoon/Desktop/rust/hoon/practice/target/debug/libpractice.rlib: /Users/yooyounghoon/Desktop/rust/hoon/practice/src/lib.rs
--- hoon/src/main.rs
+++ hoon/src/main.rs
@@ -1,3 +1,6 @@
+use practice;
+
 fn main() {
     println!("Hello, world!");
+    println!("{}",practice::add_one(10));
 }
