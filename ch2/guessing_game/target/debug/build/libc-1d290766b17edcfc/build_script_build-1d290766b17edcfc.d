3849e30d436d29a02f0274183db004a0cee424e4
younghoonyou <kcnoh2@hanyang.ac.kr>
2022-02-09 00:46:25 +0900

    Remove /debug

--- ch2/guessing_game/target/debug/build/libc-1d290766b17edcfc/build_script_build-1d290766b17edcfc.d
+++
@@ -1,5 +0,0 @@
-/Users/yooyounghoon/Desktop/rust/ch2/guessing_game/target/debug/build/libc-1d290766b17edcfc/build_script_build-1d290766b17edcfc: /Users/yooyounghoon/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.117/build.rs
-
-/Users/yooyounghoon/Desktop/rust/ch2/guessing_game/target/debug/build/libc-1d290766b17edcfc/build_script_build-1d290766b17edcfc.d: /Users/yooyounghoon/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.117/build.rs
-
-/Users/yooyounghoon/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.117/build.rs:
